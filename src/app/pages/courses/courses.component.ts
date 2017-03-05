eaf863e34d43b2dec88f515338aa86eeb34bd5e5
edonina <elena_donina@epam.com>
2017-03-06 00:02:30 +0300

    initial commit

---
+++ src/app/pages/courses/courses.component.ts
@@ -0,0 +1,38 @@
+import { Component, ViewEncapsulation, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+
+import { TodoService } from '../../core/services';
+import { TodoItem } from '../../core/entities';
+
+@Component({
+	selector: 'courses',
+	encapsulation: ViewEncapsulation.None,
+	providers: [],
+	styles: ['./courses.styles.scss'],
+	template: './courses.template.html'
+})
+export class CoursesComponent implements OnInit, OnDestroy {
+	private todoServiceSubscription: Subscription;
+	private todoList: TodoItem[];
+	private isLoading: boolean = false;
+
+	constructor(private todoService: TodoService) {
+		console.log('Home page constructor');
+
+		this.todoList = [];
+	}
+
+	public ngOnInit() {
+		console.log('Home page init');
+
+		this.isLoading = true;
+		this.todoServiceSubscription = this.todoService.getTodoItems().subscribe((res: TodoItem[]) => {
+			this.todoList = res;
+			this.isLoading = false;
+		});
+	}
+
+	public ngOnDestroy() {
+		this.todoServiceSubscription.unsubscribe();
+	}
+}
---
+++ src/app/pages/courses/courses.routes.ts
@@ -0,0 +1,9 @@
+﻿import { Routes, RouterModule } from '@angular/router';
+import { CoursesComponent }    from './courses.component.ts';
+
+// Route Configuration
+const coursesRoutes: Routes = [
+	{ path: 'courses', component: CoursesComponent },
+];
+
+export const routes = RouterModule.forChild(coursesRoutes);
