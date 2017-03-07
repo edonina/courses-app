70fdabc9074fe4fc81b2ec224e6dcbbc71a6d809
edonina <elena_donina@epam.com>
2017-03-07 14:32:51 +0300

    fix interface, make class shorter

--- src/app/core/entities/course.ts
+++ src/app/core/entities/course.ts
@@ -1,23 +1,18 @@
-interface course {
-	id: number,
-	title: string,
-	description: string,
-	creationDate: Date,
-	duration: Date
+interface ICourse {
+	id: number;
+	title: string;
+	description: string;
+	creationDate: Date;
+	duration: Date;
 }
 
-export class Course implements course{
-	public id: number;
-	public title: string;
-	public description: string;
-	public creationDate: Date;
-	public duration: Date;
-
-	constructor(id: number, title: string, description: string, creationDate: Date, duration: Date) {
-		this.id = id;
-		this.title = title;
-		this.description = description;
-		this.creationDate = creationDate;
-		this.duration = duration;
-	}
+export class Course{
+	constructor(
+		public id: number,
+		public title: string,
+		public description: string,
+		public creationDate: Date,
+		public duration: Date
+	) {}
 }
+
