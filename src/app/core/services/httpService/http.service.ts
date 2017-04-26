e5617cc0af43d20df684ced80b270d6b03b0e6c6
Elena Donina <elena_donina@epam.com>
2017-04-26 10:10:28 +0200

    clean

--- src/app/core/services/httpService/http.service.ts
+++ src/app/core/services/httpService/http.service.ts
@@ -8,32 +8,28 @@ import 'rxjs/add/operator/catch';
 export class HttpService extends Http {
 
 	constructor (backend: XHRBackend, options: RequestOptions) {
-		let token = localStorage.getItem('userToken'); // your custom token getter function here
+		let token = localStorage.getItem('userToken');
 		options.headers.set('Authorization', token);
 		super(backend, options);
 	}
 
 	request(url: string|Request, options?: RequestOptionsArgs): Observable<Response> {
 		let token = localStorage.getItem('userToken');
-		if (typeof url === 'string') { // meaning we have to add the token to the options, not in url
+		if (typeof url === 'string') {
 			if (!options) {
-				// let's make option object
 				options = {headers: new Headers()};
 			}
 			options.headers.set('Authorization', token);
 		} else {
-			// we have to add the token to the url object
 			url.headers.set('Authorization', token);
 		}
-		return super.request(url, options).catch(this.catchAuthError(this));
+		return super.request(url, options).catch(this.catchError(this));
 	}
 
-	private catchAuthError (self: HttpService) {
-		// we have to pass HttpService's own instance here as `self`
-		return (res: Response) => {
+	private catchError (self: HttpService) {
+				return (res: Response) => {
 			console.log(res);
 			if (res.status === 401 || res.status === 403) {
-				// if not authenticated
 				console.log(res);
 			}
 			return Observable.throw(res);
