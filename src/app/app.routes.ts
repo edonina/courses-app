1f3fa022d06f0c56c34465dc87239a0660bdb193
Elena Donina <elena_donina@epam.com>
2017-05-31 15:12:21 +0200

    breadcrumbs

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,5 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './';
 import { HomeComponent } from './pages/home';
 import { CoursesComponent } from './pages/courses';
 import { EditCourseComponent } from './pages/edit-course';
@@ -6,11 +7,60 @@ import { LoginComponent } from './pages/login';
 import { NoContentComponent } from './pages/no-content';
 
 export const ROUTES: Routes = [
-	{path: '', component: CoursesComponent},
+	/*{path: '', component: CoursesComponent},
 	{path: 'login', component: LoginComponent,data: {breadcrumb: "Login"}},
 	{path: 'logout', component: LoginComponent,data: {breadcrumb: "Logout"}},
 	{path: 'courses', component: CoursesComponent, data: {breadcrumb: "Courses"}},
 	{path: 'courses/:id', component: EditCourseComponent ,data: {breadcrumb: "Details"}},
 	{path: 'courses/new', component: EditCourseComponent, data: {breadcrumb: "Add course"}},
 	{path: '**', component: NoContentComponent,data: {breadcrumb: "404"}}
+*/
+	{
+		path: "",
+		component: AppComponent,
+		children: [
+			{
+				path: "login",
+				component: LoginComponent,
+				data: {
+					breadcrumb: "Login"
+				}
+			},
+			{
+				path: "",
+				component: CoursesComponent,
+				data: {
+					breadcrumb: "Courses"
+				}
+			},
+			{
+				path: "courses",
+				component: CoursesComponent,
+				data: {
+					breadcrumb: "Courses"
+				}
+			},
+			{
+				path: "courses/:id",
+				component: EditCourseComponent,
+				data: {
+					breadcrumb: "Details"
+				}
+			},
+			{
+				path: "courses/new",
+				component: EditCourseComponent,
+				data: {
+					breadcrumb: "Add"
+				}
+			},
+			{
+				path: "**",
+				component: NoContentComponent,
+				data: {
+					breadcrumb: "404"
+				}
+			}
+		]
+	},
 ];
